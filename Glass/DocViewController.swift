e5ed084f6125bbae53501dd53467a416dadd7029
Jachimike Onuoha <jachiuzo@gmail.com>
2019-07-10 21:42:49 -0700

    Working copy

---
+++ Glass/DocViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DocViewController.swift
+//  Glass
+//
+//  Created by Jachimike Onuoha on 7/10/19.
+//  Copyright © 2019 Jachimike Onuoha. All rights reserved.
+//
+
+import UIKit
+
+
+class DocViewController: UIWebView{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
--- Glass/ViewController.swift
+++ Glass/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    let pdfTitle  = "Research"
+    let docTitle = "Scripts"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    // Development in progress
-
+    
+    @IBAction func pdfButton(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: pdfTitle, withExtension: "pdf"){
+            let urlRequest = URLRequest(url: url)
+            let webView = UIWebView(frame: CGRect(x: 0, y: 20, width: 414, height: 896))
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+    }
+    
+    @IBAction func docButton(_ sender: Any) {
+    }
 }
 
